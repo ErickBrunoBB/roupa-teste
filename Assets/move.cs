86ab0f7da09e49ea4fe5651a87030b146850668e
ErickBrunoBB <67328680+ErickBrunoBB@users.noreply.github.com>
2020-06-23 11:43:44 -0300

    Initial commit

---
+++ Assets/move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            transform.position = new Vector3(0, 0.1f, 0);
+            transform.rotation = Quaternion.Euler(Vector3.forward * 0.2f);
+        }
+    }
+}
